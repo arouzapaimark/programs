078168509d6a6d1b77334ae8ac4cd1976ec853b2
Mark Arouza Pai <arouzapaimark@gmail.com>
2023-12-17 11:07:53 -0500

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,16 @@
+# Import the time module
+import time
+
+# Define main function
+def main():
+
+  # Instantiate infinite loop
+  while True:
+
+    # Use ctime to display clock
+    clock = time.ctime().split()
+    input(clock[3] + ' Press Enter to Update... ')
+
+# Call main function
+if __name__ == "__main__":
+  main()
