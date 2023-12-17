7bd0f72b28afc6855c8ab31fb8a96ba345cb4c85
Mark Arouza Pai <arouzapaimark@gmail.com>
2023-12-16 21:40:41 -0500

    Add files via upload
    Added dice table.

---
+++ dice.sql
@@ -0,0 +1,22 @@
+-- Create the table for the dice
+CREATE TABLE dice (
+  roll INT
+);
+
+-- Insert the roll data into the table
+INSERT INTO dice (roll)
+VALUES (1);
+INSERT INTO dice (roll)
+VALUES (2);
+INSERT INTO dice (roll)
+VALUES (3);
+INSERT INTO dice (roll)
+VALUES (4);
+INSERT INTO dice (roll)
+VALUES (5);
+INSERT INTO dice (roll)
+VALUES (6);
+
+-- Query random roll
+SELECT roll FROM dice
+ORDER BY RANDOM () LIMIT 1;
