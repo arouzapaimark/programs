61410eee40e7fad14360e3867a1baeaaae85f837
Mark Arouza Pai <arouzapaimark@gmail.com>
2023-12-17 11:09:40 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -9,7 +9,7 @@ def main():
 
     # Use ctime to display clock
     clock = time.ctime().split()
-    input(clock[3] + ' Press Enter to Update... ')
+    input(clock[3] + " Press Enter to Update... ")
 
 # Call main function
 if __name__ == "__main__":
